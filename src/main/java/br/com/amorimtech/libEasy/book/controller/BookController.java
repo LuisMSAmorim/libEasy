6959c291b731e7d5ababe634894914898ae19c07
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2025-08-22 15:56:34 -0300

    feat: findAll books

---
+++ src/main/java/br/com/amorimtech/libEasy/book/controller/BookController.java
@@ -0,0 +1,29 @@
+package br.com.amorimtech.libEasy.book.controller;
+
+import lombok.RequiredArgsConstructor;
+import br.com.amorimtech.libEasy.book.dto.ApiResponse;
+import br.com.amorimtech.libEasy.book.model.Book;
+import br.com.amorimtech.libEasy.book.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<Book>>> findAll(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Page<Book> bookPage = bookService.findAll(page, size);
+        return ApiResponse.success(bookPage, HttpStatus.OK).createResponseEntity();
+    }
+}
---
+++ src/main/java/br/com/amorimtech/libEasy/book/service/BookService.java
@@ -0,0 +1,21 @@
+package br.com.amorimtech.libEasy.book.service;
+
+
+import br.com.amorimtech.libEasy.book.model.Book;
+import br.com.amorimtech.libEasy.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public Page<Book> findAll(int page, int size)  {
+        Pageable pageable = PageRequest.of(page, size);
+        return bookRepository.findAll(pageable);
+    }
+}
