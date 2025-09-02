13f1f186dadd70b8f9d48e19d6b1d8f9f732d43e
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2025-09-02 14:42:00 -0300

    chore: add auth DTOs

---
+++ src/main/java/br/com/amorimtech/libEasy/auth/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package br.com.amorimtech.libEasy.auth.dto;
+
+
+public record AuthResponse(
+        String accessToken,
+        String refreshToken,
+        String tokenType
+) {
+    public static AuthResponse of(String access, String refresh) {
+        return new AuthResponse(access, refresh, "Bearer");
+    }
+}
---
+++ src/main/java/br/com/amorimtech/libEasy/auth/dto/RegisterRequest.java
@@ -0,0 +1,12 @@
+package br.com.amorimtech.libEasy.auth.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank String name,
+        @Email @NotBlank String email,
+        @Size(min=6) String password
+) {}
