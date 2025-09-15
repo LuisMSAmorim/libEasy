cedee46cec107be6abcc7f16058d6f42c11e15e3
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2025-09-15 20:36:51 -0300

    refact: remove unused code from Book model

--- src/main/java/br/com/amorimtech/libEasy/book/model/Book.java
+++ src/main/java/br/com/amorimtech/libEasy/book/model/Book.java
@@ -1,9 +1,7 @@
 package br.com.amorimtech.libEasy.book.model;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
